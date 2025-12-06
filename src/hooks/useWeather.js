617d8b734554c4f21b2041c5a28c6f35db97117a
kawser00 <access.kawser@gmail.com>
2025-12-06 14:25:38 +0600

    feat: add useWeather hook

---
+++ src/hooks/useWeather.js
@@ -0,0 +1,90 @@
+import { useState, useEffect } from "react";
+
+const useWeather = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState({
+    state: false,
+    message: "",
+  });
+  const [weatherData, setWeatherData] = useState({
+    location: "",
+    climate: "",
+    temperature: "",
+    maxTemperature: "",
+    minTemperature: "",
+    humidity: "",
+    wind: "",
+    cloudPercentage: "",
+    time: "",
+    longitude: "",
+    latitude: "",
+  });
+
+  const fetchWeatherData = async (lat, lon) => {
+    try {
+      setError(null);
+      setLoading({
+        state: true,
+        message: "Fetching weather data...",
+      });
+
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${
+          import.meta.env.VITE_WEATHER_API_KEY
+        }&units=metric`
+      );
+
+      if (!response.ok) {
+        const errorMessage = `Fetching weather data failed: ${response.status}`;
+        throw new Error(errorMessage);
+      }
+
+      const data = await response.json();
+
+      const updatedWeatherData = {
+        location: data?.name,
+        climate: data?.weather[0]?.main,
+        temperature: data?.main?.temp,
+        maxTemperature: data?.main?.temp_max,
+        minTemperature: data?.main?.temp_min,
+        humidity: data?.main?.humidity,
+        wind: data?.wind?.speed,
+        cloudPercentage: data?.clouds?.all,
+        time: data?.dt,
+        longitude: data?.coord?.lon,
+        latitude: data?.coord?.lat,
+      };
+      setWeatherData(updatedWeatherData);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading({
+        state: false,
+        message: "",
+      });
+    }
+  };
+
+  useEffect(()=> {
+    setLoading({
+      state: true,
+      message: "Fetching location...",
+    });
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        fetchWeatherData(latitude, longitude);
+      }
+    );
+
+  },[])
+
+  return {
+    weatherData,
+    error,
+    loading,
+  };
+};
+
+export default useWeather;
