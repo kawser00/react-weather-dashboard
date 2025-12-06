1265a179b0896ea15d78170c40451fd9a74c8fc7
kawser00 <access.kawser@gmail.com>
2025-12-06 22:50:02 +0600

    feat: integrate favorite location lists

--- src/components/header/FavoriteListModal.jsx
+++ src/components/header/FavoriteListModal.jsx
@@ -1,12 +1,22 @@
+import { useFavoriteContext } from '../../hooks/useFavoriteContext';
 
 function FavoriteListModal() {
+    const { favorites } = useFavoriteContext();
   return (
     <div className="max-w-xs py-4 bg-white rounded-md border-gray-500 absolute right-0 top-16 text-black shadow-lg ">
       <h3 className="text-lg font-bold px-4">Favorite Locations</h3>
       <ul className="space-y-2 mt-4 *:py-2 *:px-4 *:cursor-pointer">
-        <li className="hover:bg-gray-200">Dhaka</li>
-        <li className="hover:bg-gray-200">Rangpur</li>
-        <li className="hover:bg-gray-200">Europe</li>
+        {favorites.length > 0 ? (
+          favorites.map((fav) => (
+            <li key={fav.location} className="hover:bg-gray-200">
+              <a>
+                {fav.location}
+              </a>
+            </li>
+          ))
+        ) : (
+          <p>Nothing is added to Favorites!</p>
+        )}
       </ul>
     </div>
   );
