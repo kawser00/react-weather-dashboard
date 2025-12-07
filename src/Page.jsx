e9b08eae9e0afca67300a2e20f002ac768d17b2f
kawser00 <access.kawser@gmail.com>
2025-12-07 13:34:27 +0600

    feat: add dynamic background image

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,10 +1,43 @@
-import climateImage from "./assets/body-bg.png";
 import Header from "./components/header/Header.jsx";
 import WeatherBoard from "./components/weather/WeatherBoard.jsx";
 import { useWeatherContext } from "./hooks";
 
+import ClearSkyImage from "./assets/backgrounds/clear-sky.jpg";
+import FewCloudsImage from "./assets/backgrounds/few-clouds.jpg";
+import MistImage from "./assets/backgrounds/mist.jpeg";
+import RainyDayImage from "./assets/backgrounds/rainy-day.jpg";
+import ScatterdCloudsImage from "./assets/backgrounds/scattered-clouds.jpg";
+import SnowImage from "./assets/backgrounds/sunny.jpg";
+import ThunderStormImage from "./assets/backgrounds/thunderstorm.jpg";
+import WinterImage from "./assets/backgrounds/winter.jpg";
+
 const Page = () => {
-  const { loading } = useWeatherContext();
+  const { weatherData, loading } = useWeatherContext();
+
+  function getBackgroundImage(climate) {
+    switch (climate) {
+      case "Rain":
+        return RainyDayImage;
+      case "Clouds":
+        return ScatterdCloudsImage;
+      case "Clear":
+        return ClearSkyImage;
+      case "Snow":
+        return SnowImage;
+      case "Thunder":
+        return ThunderStormImage;
+      case "Fog":
+        return WinterImage;
+      case "Haze":
+        return FewCloudsImage;
+      case "Mist":
+        return MistImage;
+      default:
+        return ClearSkyImage;
+    }
+  }
+
+  const climateImage = getBackgroundImage(weatherData.climate);
 
   return (
     <>
@@ -15,7 +48,7 @@ const Page = () => {
       ) : (
         <div
           style={{ backgroundImage: `url('${climateImage}')` }}
-          className="grid place-items-center h-screen bg-no-repeat bg-cover text-white"
+          className="grid place-items-center h-screen bg-no-repeat bg-cover text-white bg-center"
         >
           <Header />
           <main className="w-full">
